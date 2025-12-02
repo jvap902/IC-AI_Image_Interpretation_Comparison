1594ef1771bcb3cf9c4a4cbd4bb985577b81d2d3
João Vitor Pereira <jvangelopereira@gmail.com>
2025-12-02 09:00:28 -0300

    gráfico com as duas correlações

--- plotCorrelationGraphic.py
+++ plotCorrelationGraphic.py
@@ -1,3 +1,3 @@
 from src import plot
 
-plot.correlationGraphic("dataStorage/runData.csv", "images_per_class", "spearman")
+plot.correlationGraphic("dataStorage/runData.csv", "images_per_class", "correlation", ["spearman", "pearson"])
