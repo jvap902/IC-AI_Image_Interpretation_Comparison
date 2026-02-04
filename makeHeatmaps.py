5ea92c9038f571c451a9e0e69f7c686e9a427851
jvap902 <jvangelopereira@gmail.com>
2026-02-03 22:40:23 -0300

    correção dinov3

--- makeHeatmaps.py
+++ makeHeatmaps.py
@@ -1,6 +1,11 @@
 from src.plot import heatMap
 
-dataset = 'fgvc-aircraft'
-
-heatMap(f'./dataStorage/results/{dataset}Data.csv', 'pearson')
-heatMap(f'./dataStorage/results/{dataset}Data.csv', 'spearman')
+datasets = ['timm/mini-imagenet', 'imagenet-sketch', 'cifar10', 'cifar100', 'fgvc-aircraft']
+    
+    
+for dt in datasets:
+    
+    dataset = dt.replace('/', '-')
+    
+    heatMap(f'./dataStorage/results/{dataset}Data.csv', 'pearson')
+    heatMap(f'./dataStorage/results/{dataset}Data.csv', 'spearman')
