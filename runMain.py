6408981de505a09aa249094c63004a24295cab82
jvap902 <jvangelopereira@gmail.com>
2026-04-22 20:45:07 -0300

    fim cka imagenet-sketch

--- runMain.py
+++ runMain.py
@@ -155,8 +155,8 @@ if __name__ == "__main__":
         case _:
             raise
         
-    fst_idx = codToInstance(15, 'a')[0]
-    snd_idx = codToInstance(16, 'c')[0]+1
+    fst_idx = codToInstance(15, 'a')[0]+1
+    snd_idx = fst_idx+1 #codToInstance(16, 'c')[0]
         
     start_params = {'fst_instance': fst_idx, 'snd_instance': snd_idx, 'dataset': 0, 'interrupt': (-1, -1, 6)}
     
