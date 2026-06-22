d9e68f2d0814f4bc3f4e929a4096d013a6e51c4a
jvap902 <jvangelopereira@gmail.com>
2026-06-22 18:54:03 -0300

    resultados cka

--- runMain.py
+++ runMain.py
@@ -159,9 +159,9 @@ if __name__ == "__main__":
         case _:
             raise
         
-    fst_idx = codToInstance(5, 'd')[0]
-    snd_idx = codToInstance(12, 'a')[0]+1
-    ini_dt = datasets.index(('imagenet-c-fog-5', 0))
+    fst_idx = codToInstance(12, 'a')[0]
+    snd_idx = codToInstance(21, 'a')[0]+1
+    ini_dt = datasets.index(('imagenet-c-jpeg_compression-5', 0))
     
     fst_model_interr = codToInstance(16, 'c')[0]
     snd_model_interr = codToInstance(17, 'a')[0]
